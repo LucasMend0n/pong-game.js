582eeec22199eb89f2d804b70ebededd56def1ef
Lucas Mendonça <lucasjr413@gmail.com>
2022-06-04 22:24:02 -0300

    exibindo na tela os elementos do jogo

--- pong.js
+++ pong.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("pong");
-const contexto = canvas.getContext("2d");
+const context = canvas.getContext("2d");
 
 canvas.width = 1000; 
-canvas.height = 750; 
+canvas.height = 800; 
 
 let pontosP1 = 0; 
 let pontosP2 = 0; 
@@ -17,43 +17,79 @@ class Element{
         this.speed = options.x || 2;
         this.gravity = options.x;
     }
-
-
 }
 
 //primeiro marcador
 const jogador1 = new Element({
     x: 10, 
-    y: 200, 
-    width: 15, 
-    height: 80, 
+    y: canvas.height/2, 
+    width: 20, 
+    height: 100, 
     color: "#ffffff", 
     gravity: 2, 
 });
 //segundo marcador
 const jogador2 = new Element({
-    x: 650, 
-    y: 200, 
-    width: 15, 
-    height: 80, 
+    x: 970, 
+    y: canvas.height/2, 
+    width: 20, 
+    height: 100, 
     color: "#ffffff", 
     gravity: 2, 
 });
 //bola
 
-//jogador 1 pontuação 
+const bola = new Element({
+    x: 1000/2, 
+    y: 800/2, 
+    width: 30, 
+    height: 30, 
+    color: "red", 
+    gravity: 1,
+    speed: 1, 
+});
+
+//cria elemento
+function criaElemento(elemento){
+    context.fillStyle = elemento.color; 
+    context.fillRect(elemento.x, elemento.y, elemento.width, elemento.height); 
+}
+
+//jogador 1 pontuação
+
+function exibePontuacao1(){
+    context.font = '36px Arial';
+    context.fillStyle = "#fff";
+    context.fillText(pontosP1, canvas.width/2-60,30); 
+}
 
 //jogador 2 pontuação 
 
-// mostra elementos na tela
-
-function desenhaElemento(elemento){
-    contexto.fillStyle = elemento.color; 
-    contexto.fillRect(elemento.x, elemento.y, elemento.width, elemento.height); 
-
+function exibePontuacao2(){
+    context.font = '36px Arial';
+    context.fillStyle = "#fff";
+    context.fillText(pontosP2, canvas.width/2+60,30); 
 }
-desenhaElemento(jogador1)
-//detecta colisao
+
+// mostra elemento na tela
+
+function mostraElementos(){
+    context.clearRect(0,0, canvas.width,canvas.height)
+    criaElemento(jogador1);
+    criaElemento(jogador2); 
+    criaElemento(bola);
+    exibePontuacao1(); 
+    exibePontuacao2(); 
+}
+
+//loop do jogo
+
+function loop(){
+    mostraElementos()
+    window.requestAnimationFrame(loop)
+}
+
+loop()
 
 
 
